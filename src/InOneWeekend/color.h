f5e3c1180b91f4352aa39f57812995260e447649
unknown <yinma2448@gmail.com>
2024-09-16 12:12:56 +0800

    add gamma

--- src/InOneWeekend/color.h
+++ src/InOneWeekend/color.h
@@ -6,11 +6,24 @@
 
 using color = vec3;
 
+inline double linearToGamma(double linearComponent)
+{
+    if (linearComponent > 0)
+    {
+        return std::sqrt(linearComponent);
+    }
+    return 0;
+}
+
 void writeColor(const color& pixelColor) 
 {
     auto r = pixelColor.x();
     auto g = pixelColor.y();
     auto b = pixelColor.z();
+
+    r = linearToGamma(r);
+    g = linearToGamma(g);
+    b = linearToGamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     int rbyte = int(255.999 * r);
