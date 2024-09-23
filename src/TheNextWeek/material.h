5e1f8dae5858b3045deba3678b0b780e3191d8e1
unknown <yinma2448@gmail.com>
2024-09-23 11:44:39 +0800

    add texture

--- src/TheNextWeek/material.h
+++ src/TheNextWeek/material.h
@@ -12,6 +12,7 @@
 //==============================================================================================
 
 #include "hittable.h"
+#include "texture.h"
 
 
 class material {
@@ -28,7 +29,10 @@ class material {
 
 class lambertian : public material {
   public:
-    lambertian(const color& albedo) : albedo(albedo) {}
+    lambertian(const color& albedo) : tex(make_shared<solid_color>(albedo)) {}
+
+    lambertian(shared_ptr<texture> tex) : tex(tex) {}
+
 
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override {
@@ -39,12 +43,12 @@ class lambertian : public material {
             scatter_direction = rec.normal;
 
         scattered = ray(rec.p, scatter_direction, r_in.time());
-        attenuation = albedo;
+        attenuation = tex->value(rec.u, rec.v, rec.p);
         return true;
     }
 
   private:
-    color albedo;
+      shared_ptr<texture> tex;
 };
 
 
---
+++ src/TheNextWeek/texture.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "rtweekend.h"
+
+class texture {
+public:
+	virtual ~texture() = default;
+
+	virtual color value(double u, double v, const point3& p) const = 0;
+};
+
+
+
+
+class solid_color : public texture {
+public:
+	solid_color(const color& albedo) : albedo(albedo) {}
+
+	solid_color(double red, double green, double blue) : solid_color(color(red, green, blue)) {}
+
+	color value(double u, double v, const point3& p) const override {
+		return albedo;
+	}
+
+private:
+	color albedo;
+
+};
+
+
+class checker_texture : public texture {
+public:
+	checker_texture(double scale, shared_ptr<texture> even, shared_ptr<texture> odd) 
+		: inv_scale(1.0/scale), even(even), odd(odd) {}
+
+	checker_texture(double scale, const color& c1, const color& c2) 
+		: checker_texture(scale, make_shared<solid_color>(c1), make_shared<solid_color>(c2)) {}
+
+	color value(double u, double v, const point3& p) const override {
+		auto xInteger = int(std::floor(inv_scale * p.x()));
+		auto yInteger = int(std::floor(inv_scale * p.y()));
+		auto zInteger = int(std::floor(inv_scale * p.z()));
+
+		bool isEven = (xInteger + yInteger + zInteger) % 2 == 0;
+
+		return isEven ? even->value(u, v, p) : odd->value(u, v, p);
+	}
+
+
+private:
+	double inv_scale;
+	shared_ptr<texture> even;
+	shared_ptr<texture> odd;
+};
