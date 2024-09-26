9574ddb41bc93da6dfdea8e333cb2dbd7862ddab
unknown <yinma2448@gmail.com>
2024-09-26 10:24:24 +0800

    add instance

--- src/TheNextWeek/main.cc
+++ src/TheNextWeek/main.cc
@@ -252,10 +252,20 @@ void cornell_box() {
     world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
     world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
 
+    shared_ptr<hittable> box1 = box(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+    world.add(box1);
+
+    shared_ptr<hittable> box2 = box(point3(0, 0, 0), point3(165, 165, 165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+    world.add(box2);
+
     camera cam;
 
     cam.aspect_ratio = 1.0;
-    cam.image_width = 600;
+    cam.image_width = 400;
     cam.samples_per_pixel = 200;
     cam.max_depth = 50;
     cam.background = color(0, 0, 0);
@@ -269,7 +279,6 @@ void cornell_box() {
 
     cam.render(world);
 }
-
 
 
 int main() {
--- src/TheNextWeek/quad.h
+++ src/TheNextWeek/quad.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "hittable.h"
+#include "hittable_list.h"
 #include "rtweekend.h"
 
 
@@ -74,3 +75,25 @@ private:
     vec3 normal;
     double D;
 };
+
+
+
+inline shared_ptr<hittable_list> box(const point3& a, const point3& b, shared_ptr<material> mat) {
+    auto sides = make_shared<hittable_list>();
+
+    auto min = point3(std::fmin(a.x(), b.x()), std::fmin(a.y(), b.y()), std::fmin(a.z(), b.z()));
+    auto max = point3(std::fmax(a.x(), b.x()), std::fmax(a.y(), b.y()), std::fmax(a.z(), b.z()));
+
+    auto dx = vec3(max.x() - min.x(), 0, 0);
+    auto dy = vec3(0, max.y() - min.y(), 0);
+    auto dz = vec3(0, 0, max.z() - min.z());
+
+    sides->add(make_shared<quad>(point3(min.x(), min.y(), max.z()), dx, dy, mat)); // front
+    sides->add(make_shared<quad>(point3(max.x(), min.y(), max.z()), -dz, dy, mat)); // right
+    sides->add(make_shared<quad>(point3(max.x(), min.y(), min.z()), -dx, dy, mat)); // back
+    sides->add(make_shared<quad>(point3(min.x(), min.y(), min.z()), dz, dy, mat)); // left
+    sides->add(make_shared<quad>(point3(min.x(), max.y(), max.z()), dx, -dz, mat)); // top
+    sides->add(make_shared<quad>(point3(min.x(), min.y(), min.z()), dx, dz, mat)); // bottom
+
+    return sides;
+}
